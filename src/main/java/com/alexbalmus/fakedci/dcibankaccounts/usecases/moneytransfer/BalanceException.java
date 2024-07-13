0434a777117d024889a9e55654c6f484832ca386
alexbalmus <alexbalmus@gmail.com>
2024-07-13 11:47:50 +0300

    Project moved

--- src/main/java/com/alexbalmus/fakedci/dcibankaccounts/usecases/moneytransfer/BalanceException.java
+++
@@ -1,9 +0,0 @@
-package com.alexbalmus.fakedci.dcibankaccounts.usecases.moneytransfer;
-
-class BalanceException extends RuntimeException
-{
-    public BalanceException(final String message)
-    {
-        super(message);
-    }
-}
